41e4fbd84bbbc8644d934d5f4d23a11f9584830e
Young Han <younghan@meta.com>
2025-04-07 20:10:33 -0700

    style: rm node background color

--- end-to-end-use-cases/long_context/book-character-mindmap/src/pages/bookPage/components/CharacterGraph.jsx
+++ end-to-end-use-cases/long_context/book-character-mindmap/src/pages/bookPage/components/CharacterGraph.jsx
@@ -46,13 +46,6 @@ export default function CharacterGraph({ graphData }) {
               (n) => n + fontSize * 0.2
             );
 
-            ctx.fillStyle = "rgba(255, 255, 255, 0.8)";
-            ctx.fillRect(
-              node.x - bckgDimensions[0] / 2,
-              node.y - bckgDimensions[1] / 2,
-              ...bckgDimensions
-            );
-
             ctx.textAlign = "center";
             ctx.textBaseline = "middle";
             ctx.fillStyle = "#3b82f6";
