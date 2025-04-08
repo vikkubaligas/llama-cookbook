75ccc95cdc70e31ecfb6434d5f868d303028ddc6
Young Han <younghan@meta.com>
2025-04-08 01:04:42 -0700

    rm: unused AISearch

--- end-to-end-use-cases/long_context/book-character-mindmap/src/pages/bookPage/index.jsx
+++ end-to-end-use-cases/long_context/book-character-mindmap/src/pages/bookPage/index.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import { FaSearch, FaBook, FaHome } from "react-icons/fa";
 import { Link } from "react-router-dom";
-import AISearch from "./components/AISearch";
 import CharacterGraph from "./components/CharacterGraph";
 import Together from "together-ai";
 
